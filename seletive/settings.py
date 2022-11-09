b8cd680bf1415c1a596335753e21ab5bc5c64685
yankniess <you@example.com>
2022-11-09 19:02:50 -0300

    modified:   db.sqlite3 	modified:   empresa/__pycache__/admin.cpython-311.pyc 	modified:   empresa/__pycache__/models.cpython-311.pyc 	modified:   empresa/__pycache__/views.cpython-311.pyc 	modified:   empresa/admin.py 	new file:   empresa/migrations/0001_initial.py 	new file:   empresa/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   empresa/models.py 	modified:   empresa/templates/nova_empresa.html 	modified:   empresa/views.py 	modified:   seletive/__pycache__/settings.cpython-311.pyc 	modified:   seletive/settings.py

--- seletive/settings.py
+++ seletive/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -128,3 +129,11 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#mensagens
+MESSAGE_TAGS = {
+    constants.DEBUG:'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
